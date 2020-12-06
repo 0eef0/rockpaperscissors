9d6265fc8578656a7d27320aa60e1bd179bc646a
TheCapn-MEC <erolda266@west-mec.org>
2020-12-06 15:00:37 -0700

    added improvements

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,20 @@ function getSiteChoice(goodChoice){
         switch(goodChoice){
             case "rock":
                 document.getElementById("exclaim").innerHTML = "PAPER!!!";
+                paper.play();
+                setTimeout(() => {  document.getElementById("exclaim").innerHTML = ""; }, 1000);
                 return "paper";
                 break;
             case "paper":
                 document.getElementById("exclaim").innerHTML = "SCISSORS!";
+                scissors.play();
+                setTimeout(() => {  document.getElementById("exclaim").innerHTML = ""; }, 1000);
                 return "scissors";
                 break;
             case "scissors":
                 document.getElementById("exclaim").innerHTML = "ROCK!!!";
+                rock.play();
+                setTimeout(() => {  document.getElementById("exclaim").innerHTML = ""; }, 1000);
                 return "rock";
                 break;
         }
@@ -26,16 +32,22 @@ function getSiteChoice(goodChoice){
             case 1:
                 console.log("rock");
                 document.getElementById("exclaim").innerHTML = "ROCK!!!";
+                rock.play();
+                setTimeout(() => {  document.getElementById("exclaim").innerHTML = ""; }, 1000);
                 return "rock";
                 break;
             case 2:
                 console.log("paper");
                 document.getElementById("exclaim").innerHTML = "PAPER!!!";
+                paper.play();
+                setTimeout(() => {  document.getElementById("exclaim").innerHTML = ""; }, 1000);
                 return "paper";
                 break;
             case 3:
                 console.log("scissors");
                 document.getElementById("exclaim").innerHTML = "SCISSORS!";
+                scissors.play();
+                setTimeout(() => {  document.getElementById("exclaim").innerHTML = ""; }, 1000);
                 return "scissors";
                 break;
         }
@@ -140,6 +152,20 @@ var game = '<img src="images/computer.png" id="theCPU">'+
 '            <td colspan="2"><h2 id="result"></h2></td>'+
 '        </tr>'+
 '    </table>';
+
+var gameTable = '    <table>'+
+'        <tr>'+
+'            <td><h2>Player Score</h2></td>'+
+'            <td><h2>Computer Score</h2></td>'+
+'        </tr>'+
+'        <tr>'+
+'            <td><h2 id="player"></h2></td>'+
+'            <td><h2 id="computer"></h2></td>'+
+'        </tr>'+
+'        <tr>'+
+'            <td colspan="2"><h2 id="result"></h2></td>'+
+'        </tr>'+
+'    </table>';
 	
 function startGame(rounds){
     checkDifficulty();
@@ -150,13 +176,23 @@ function startGame(rounds){
 //the function and variable below end the game after someone wins the game
 var totalRounds = 0;
 function checkForWin(){
-    if(playerScore == Math.ceil(totalRounds / 2) || computerScore == Math.ceil(totalRounds / 2)){
-        if(playerScore > computerScore){
-            document.getElementById("mainDiv").innerHTML = "<h2>Player WIN!</h2>";
-        }else{
-            document.getElementById("mainDiv").innerHTML = "<h2>Computer WIN!</h2>";
+    setTimeout(() => {  
+        if(playerScore == Math.ceil(totalRounds / 2) || computerScore == Math.ceil(totalRounds / 2)){
+            if(playerScore > computerScore){
+                document.getElementById("mainDiv").innerHTML = "<h2>Player WIN!</h2>" + gameTable;
+                document.getElementById("player").innerHTML = playerScore;
+                document.getElementById("computer").innerHTML = computerScore;
+                document.getElementById("result").innerHTML = "<button>Reset</button>";
+                document.getElementById("result").innerHTML = "<button onclick='resetGame()'>Reset</button>";
+            }else{
+                document.getElementById("mainDiv").innerHTML = "<h2>Computer WIN!</h2>" + gameTable;
+                document.getElementById("player").innerHTML = playerScore;
+                document.getElementById("computer").innerHTML = computerScore;
+                document.getElementById("result").innerHTML = "<button onclick='resetGame()'>Reset</button>";
+            }
         }
-    }
+    }, 1000);
+
 }
 
 //Below is the code for hard mode which will totally be fair
@@ -166,3 +202,18 @@ function checkDifficulty(){
         hardMode = true;
     }
 }
+
+//function and variable below resets the game
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById('mainDiv').innerHTML = startMenu;
+    hardMode = false;
+}
+var startMenu = '<h2>How would you like to play?</h2>'+
+'        <button onclick="startGame(3)">Best of 3</button>'+
+'        <button onclick="startGame(5)">Best of 5</button>'+
+'        <button onclick="startGame(7)">Best of 7</button>'+
+''+
+'        <input type="checkbox" id="diffCheck">'+
+'        <label for="diffCheck">Hard Mode</label>';
